9ff28373da1d1d1916f78d768cea1679964fb92f
Łukasz Wójcik <1711174+lwojcik@users.noreply.github.com>
2023-07-22 18:10:52 +0200

    Update

--- _11ty/helpers/addRef.js
+++ _11ty/helpers/addRef.js
@@ -3,7 +3,7 @@ const siteConfig = require("../../content/_data/siteConfig");
 const REFERRER_URL = siteConfig.ref;
 
 module.exports = (url) => {
-  // if (!siteConfig.addRefToExternalLinks) return url;
+  if (!siteConfig.addRefToExternalLinks) return url;
 
   const urlObject = new URL(url);
   urlObject.searchParams.append("ref", REFERRER_URL);
